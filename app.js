30fc9c67a51e05da2cee1ecbf7eb0622e6e2c0d8
John <a0972199950@gmail.com>
2018-12-28 12:37:05 +0800

    完成app.js

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
 var todoData = [
-    {
-        content: "吃午餐",
-        createdAt: 1
-    },
-    {
-        content: "吃晚餐",
-        createdAt: 1000
-    }
 ];
 
 // 讀取資料
 function render(todoData){
     // 把資料渲染到螢幕上
     var $ul = $("ul");
-    var HTML = "";
+    var HTML = "";    
 
     for(var i = 0; i < todoData.length; i++){
         HTML = HTML + `
-        <li>
+        <li id="${todoData[i].id}">
             <span class="delete">
                 刪除
             </span>
@@ -25,6 +17,9 @@ function render(todoData){
             ${moment(todoData[i].createdAt).format("MM/DD hh:mm")}
         </li>`;
     };
+
+    // 清空畫面
+    $("ul").empty();
 
     $ul.append(HTML);
 
@@ -44,15 +39,13 @@ $("#addTodoBtn").on("click", function(event){
 
     // 把使用者輸入的資料存進todoData
     var newTodoData = {
+        id: uuid(),
         content: newTodoText,
         createdAt: moment().valueOf()
     };
 
-    todoData.push(newTodoData);
 
-    
-    // 清空畫面
-    $("ul").empty();
+    todoData.push(newTodoData);
 
     // 再次render todoData
     render(todoData);
@@ -65,6 +58,15 @@ $("#addTodoBtn").on("click", function(event){
 
 
 $("ul").on("click", ".delete", function(){
-    // 我要刪掉誰？
-    $(this).parent("li").remove();
+    // 先刪掉資料庫裡的資料
+    var idToDelete = $(this).parent("li").attr("id");
+    
+    todoData = todoData.filter(function(todo){
+        if(todo.id === idToDelete) return false;
+        else return true;        
+    });
+
+    // 重新rander一次
+    render(todoData);
+
 });
