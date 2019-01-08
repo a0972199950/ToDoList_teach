70f70e9c5e235c477a7d3f9f3379d377b85bfbbe
John <a0972199950@gmail.com>
2019-01-08 10:42:27 +0800

    0108

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 var todoData = [
+    {
+        id: uuid(),
+        content: "aaa",
+        createdAt: 34253442
+    },
+    {
+        id: uuid(),
+        content: "bbb",
+        createdAt: 235435
+    }
 ];
+
+var errorMsgData = "";
+
+function renderErr(){
+    //render err
+    var $errorMsg = $("#errorMsg");
+    var $addTodoInput = $("#addTodoInput");
+
+    $errorMsg.text("");   
+
+    $errorMsg.append(errorMsgData);
+
+    $addTodoInput.val("");
+    errorMsgData = "";
+}
 
 // 讀取資料
 function render(todoData){
-    // 把資料渲染到螢幕上
+
+    var $addTodoInput = $("#addTodoInput");
+    var $errorMsg = $("#errorMsg");
+
+    
+    // render ul
     var $ul = $("ul");
-    var HTML = "";    
+    var HTML = "";
 
     for(var i = 0; i < todoData.length; i++){
         HTML = HTML + `
@@ -20,9 +50,34 @@ function render(todoData){
 
     // 清空畫面
     $("ul").empty();
+    $errorMsg.text("");
 
     $ul.append(HTML);
 
+    
+    $addTodoInput.val(""); 
+    errorMsgData = "";
+};
+
+
+function validate(newTodoText){
+    var isValid = true;
+
+    // 第一關，驗證是否空值，若是則把isValid改成false
+    if(!newTodoText){
+        isValid = false;
+        errorMsgData = "請輸入內容";
+    }
+
+    // 第二關，驗證是否重複，若是則把isValid改成false
+    for(var i = 0; i < todoData.length; i++){
+        if(todoData[i].content === newTodoText){
+            isValid = false;
+            errorMsgData = "輸入的值已存在";
+        };
+    };
+
+    return isValid;
 };
 
 render(todoData);
@@ -34,8 +89,11 @@ $("#addTodoBtn").on("click", function(event){
     var $addTodoInput = $("#addTodoInput");
     var newTodoText = $addTodoInput.val().trim();
 
-    // 如果使用者啥都沒輸入
-    if(!newTodoText) return;
+    var isValid = validate(newTodoText);
+    if(!isValid){
+        renderErr();
+        return;
+    }
 
     // 把使用者輸入的資料存進todoData
     var newTodoData = {
@@ -49,11 +107,7 @@ $("#addTodoBtn").on("click", function(event){
 
     // 再次render todoData
     render(todoData);
-
-
-    // $("ul").append(`<li><span class="delete">刪除</span>${newTodoText}${moment().format("MM/DD hh:mm")}</li>`);
-
-    $addTodoInput.val("");
+    
 });
 
 
